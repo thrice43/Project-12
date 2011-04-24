dfbf1193f41ea1ab4e99d299921f7310434a874a
James <thrice43@gmail.com>
2011-04-24 12:48:08 -0400

    added template .cpp

---
+++ .cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+// Prototypes
+
+
+
+// Main Function
+int main(){
+    //Code...
+}
+
+
+// Prototype Definition
